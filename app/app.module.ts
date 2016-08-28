343465eb55c6b19028e3d845d2be01168c604b69
Thanh Truong <thanh.v.truong@gmail.com>
2016-08-28 02:47:46 -0700

    added routing, need to Add a Dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
-    AppComponent,
-    HeroDetailComponent
+    AppComponent,    
+    HeroDetailComponent,
+    HeroesComponent
+  ],  
+  providers: [
+      HeroService
   ],
   bootstrap: [ AppComponent ]
 })
